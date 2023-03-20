56be4c0722df087c1ec8af7f978661bee7babfac
David Corona <corona_121@hotmail.com>
2023-03-20 20:12:03 +0100

    Post-list - Output list of posts (Structural directives)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 import { HeaderComponent } from './header/header.component';
 
 
@@ -16,6 +18,7 @@ import { HeaderComponent } from './header/header.component';
   declarations: [
     AppComponent,
     PostCreateComponent,
+    PostListComponent,
     HeaderComponent
   ],
   imports: [
@@ -25,7 +28,8 @@ import { HeaderComponent } from './header/header.component';
     MatInputModule,
     MatButtonModule,
     MatCardModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
