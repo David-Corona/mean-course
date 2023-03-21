fd2749b3ddd10040754f981de90a5b98dad7cfbe
David Corona <corona_121@hotmail.com>
2023-03-21 13:33:58 +0100

    Create Post with Forms - ngForm, ngModel, validation

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Output } from "@angular/core";
-
+import { NgForm } from "@angular/forms";
 
 import { Post } from '../post.model';
 
@@ -10,14 +10,15 @@ import { Post } from '../post.model';
 })
 export class PostCreateComponent {
 
-  enteredTitle = "";
-  enteredContent = "";
-  @Output() postCreated = new EventEmitter();
+  @Output() postCreated = new EventEmitter<Post>();
 
-  onAddPost() {
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
     const post: Post = {
-      title: this.enteredTitle,
-      content: this.enteredContent
+      title: form.value.title,
+      content: form.value.content
     };
     this.postCreated.emit(post);
   }
