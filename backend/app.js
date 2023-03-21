4cf668e6ee1bf3debef5a0ab2177c52a50b904c4
David Corona <corona_121@hotmail.com>
2023-03-21 20:05:42 +0100

    CORS - Set Headers

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,18 @@ const express = require('express'); // express package installed
 
 const app = express(); // execute the package as a function, and it will return an express app
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Allow any domain to access
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Request-With, Content-Type, Accept'
+  ); // Incoming requests may have these headers, others will be blocked.
+  res.setHeader(
+    'Accese-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
+  ); // Only allows these methods, always add options
+  next();
+});
 
 app.use('/api/posts', (req, res, next) => {
 
