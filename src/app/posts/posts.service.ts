ce50ca78eecb92efa752f466a5032472c18df3f5
David Corona <corona_121@hotmail.com>
2023-03-21 14:37:52 +0100

    Post Service - Lifecycle hooks, Observables

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,19 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 import { Post } from "./post.model";
 
-@Injectable()
+@Injectable({providedIn: 'root'})
 export class PostsService {
 
   private posts: Post[] = [];
+  private postsUpdated = new Subject<Post[]>(); // like an event emitter
 
   getPosts() {
     return [...this.posts]; //copies the array
   }
 
+  // returns object which we can listen, but not emit. We can emit only from this file.
+  getPostUpdateListener() {
+    return this.postsUpdated.asObservable();
+  }
+
   addPosts(title: string, content: string) {
     const post: Post = {title: title, content: content};
     this.posts.push(post);
+    this.postsUpdated.next([...this.posts]); // emits a new value, which is a copy of posts
   }
 
 }
