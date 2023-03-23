6f5827db8bd681c31ab75492269457a291b8ad09
David Corona <corona_121@hotmail.com>
2023-03-23 19:08:10 +0100

    Edit form - angular and server

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -36,6 +36,11 @@ export class PostsService {
     return this.postsUpdated.asObservable();
   }
 
+  getPost(id: string) {
+    // return {...this.posts.find(p => p.id === id)}; // return clone of the received object if coincides with id received
+    return this.http.get<{_id: string, title: string, content: string}>("http://localhost:3000/api/posts/" + id);
+  }
+
   addPosts(title: string, content: string) {
     const post: Post = {title: title, content: content};
     this.http.post<{message: string, postId: string}>('http://localhost:3000/api/posts', post)
@@ -44,6 +49,18 @@ export class PostsService {
         post.id = id;
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]); // emits a new value, which is a copy of posts
+      });
+  }
+
+  updatePost(id: string, title: string, content: string) {
+    const post: Post = { id: id, title: title, content: content};
+    this.http.put("http://localhost:3000/api/posts/" + id, post)
+      .subscribe(() => {
+        const updatedPosts = [...this.posts];
+        const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+        updatedPosts[oldPostIndex] = post;
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts]);
       });
   }
 
