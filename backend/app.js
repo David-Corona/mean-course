d681a7b9c5dab30c08583d0bce7bbbd3c1c0b6f0
David Corona <corona_121@hotmail.com>
2023-03-23 19:16:27 +0100

    Reorganized backend routes

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,7 @@ const express = require('express'); // express package installed
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 
-const Post = require('./models/post');
+const postsRoutes = require('./routes/posts');
 
 const app = express(); // execute the package as a function, and it will return an express app
 
@@ -30,59 +30,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post('/api/posts', (req, res, next) => {
-  const post = new Post({
-    title: req.body.title, // body is the new field added by body-parser
-    content: req.body.content
-  });
-  post.save().then(createdPost => {
-    res.status(201).json({
-      message: 'Post added successfully!',
-      postId: createdPost._id
-    });
-  });
-});
-
-app.put("/api/posts/:id", (req, res, next) => {
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content
-  });
-  Post.updateOne({_id: req.params.id}, post).then(result => {
-    console.log(result);
-    res.status(200).json({message: 'Updated successfully!'});
-  });
-});
-
-app.get('/api/posts', (req, res, next) => {
-  Post.find() // static method
-    .then(documents => {
-      res.status(200).json({
-        message: 'Posts fetched successfully!',
-        posts: documents
-      });
-    });
-});
-
-app.get('/api/posts/:id', (req, res, next) => {
-  Post.findById(req.params.id)
-    .then(post => {
-      if (post) {
-        res.status(200).json(post);
-      } else {
-        res.status(404).json({message: "Post not found!"});
-      }
-    });
-});
-
-app.delete('/api/posts/:id', (req, res, next) => {
-  console.log(req.params.id); // params: Express property that gives access to parameters
-  Post.deleteOne({ _id: req.params.id}).then(result => {
-    console.log(result);
-    res.status(200).json({ message: 'Post deleted!'});
-  });
-});
+app.use("/api/posts", postsRoutes);
 
 // export the app (and import it from server.js)
 module.exports = app; // will export the app and all the middlewares attached to it
