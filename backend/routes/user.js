475f4bf9f4891fbb17b36cc967755d1582d8fdd6
David Corona <corona_121@hotmail.com>
2023-03-27 21:01:03 +0200

    Auth - SPA auth, creating and sending token

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
@@ -28,6 +29,39 @@ router.post("/signup", (req, res, next) => {
     });
 });
 
+router.post("/login", (req, res, next) => {
+  let fetchedUser;
+  User.findOne({ email: req.body.email})
+    .then(user => {
+      if (!user) { // if user doesn't exist, undefined
+        return res.status(401).json({
+          message: "Auth failed!"
+        });
+      }
+      fetchedUser = user;
+      return bcrypt.compare(req.body.password, user.password); // compare password is correct
+    })
+    .then(result => { // result will be true/false depending on previous compare
+      if (!result) {
+        return res.status(401).json({
+          message: "Auth failed, incorrect credentials."
+        });
+      }
+      const token = jwt.sign( // creates new token
+        { email: fetchedUser.email, userId: fetchedUser._id},
+        'secret_this_should_be_longer', // string used to hash, should be a very long string
+        { expiresIn: "1h"}
+      );
+      res.status(200).json({
+        token: token
+      })
+    })
+    .catch(err => {
+      return res.status(401).json({
+        message: "Auth failed!"
+      });
+    })
+});
 
 
 module.exports = router;
