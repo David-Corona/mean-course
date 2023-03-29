1f2edfd1209b6f4375d7aaf5a0b329f803d7d261
David Corona <corona_121@hotmail.com>
2023-03-29 17:26:29 +0200

    Optimization in front - Splitting modules (angular material, posts and auth)

---
+++ src/app/angular-material.module.ts
@@ -0,0 +1,27 @@
+// this simply imports from angular material and exports them
+import { NgModule } from "@angular/core";
+
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatDialogModule } from '@angular/material/dialog';
+
+
+@NgModule({
+  // imports: [...] => import is done automatically by angular
+  exports: [ // export to make available to import from app.module
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+    MatDialogModule
+  ]
+})
+export class AngularMaterialModule {}
